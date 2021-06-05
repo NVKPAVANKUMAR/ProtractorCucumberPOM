9c7de571579a6b22243ebf353ac1537fa2a07a66
NVKPAVANKUMAR <npavankumar144@gmail.com>
2021-06-05 20:11:51 +0530

    removed node modules from github

--- report-config.js
+++ report-config.js
@@ -1,19 +1,19 @@
 var reporter = require('cucumber-html-reporter');
 
 var options = {
-    theme: 'bootstrap',
-    jsonFile: './reports/cucumber_report.json',
-    output: './reports/cucumber_report.html',
-    reportSuiteAsScenarios: true,
-    launchReport: true,
-    metadata: {
-        "App Version":"0.3.2",
-        "Test Environment": "Prod Env",
-        "Browser": "Chrome  54.0.2840.98",
-        "Platform": "Windows 7",
-        "Parallel": "Scenarios",
-        "Executed": "Remote"
-    }
+  theme: 'bootstrap',
+  jsonFile: './reports/cucumber_report.json',
+  output: './reports/cucumber_report.html',
+  reportSuiteAsScenarios: true,
+  launchReport: true,
+  metadata: {
+    'App Version': '0.3.2',
+    'Test Environment': 'Prod Env',
+    Browser: window.navigator.platform,
+    Platform: window.navigator.os,
+    Parallel: 'Scenarios',
+    Executed: 'Remote',
+  },
 };
 
 reporter.generate(options);
